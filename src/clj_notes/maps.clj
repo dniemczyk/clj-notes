5efd1dc66d64d82f9abdb6f2131f752165422b14
Damian Niemczyk <djniemczyk7@googlemail.com>
2015-09-28 09:03:59 +0200

    Fix comments to reflect true REPL output

--- src/clj_notes/maps.clj
+++ src/clj_notes/maps.clj
@@ -3,4 +3,6 @@
 ;; commas are treated as whitespace, not only for maps
 ;; but it is interesting that the response will be comma
 ;; seperated
-(def a-map {:a 1 :b 2 :c 3}) ; => {:a 1, :b 2, :c 3}
+(def a-map {:a 1 :b 2 :c 3}) ;=> #'clj-notes.maps/a-map
+
+(conj a-map [:d 4]) ; => {:a 1, :b 2, :c 3, :d 4}
