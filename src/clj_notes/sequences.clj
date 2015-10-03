b9ca671266546a8bbed27bbc15081aa62ca804b2
Damian Niemczyk <djniemczyk7@googlemail.com>
2015-10-03 19:38:55 +0200

    Extend the notes on seq abstraction

--- src/clj_notes/sequences.clj
+++ src/clj_notes/sequences.clj
@@ -1,4 +1,21 @@
+;;;; The Sequence abstraction
 (ns clj-notes.sequences)
+
+;;; Seqs define define a way to obtain and traverse sequential
+;;; views of some sort of values. The key functions working on
+;;; seqs are:
+;;;
+;;; seq - produces a sequence over its arguments
+;;; first, last, next - provide ways to consume seqs
+;;; lazy-seq - produces a lazy sequence
+
+;; seq usage examples:
+(seq "Clojure")   ; => (\C \l \o \j \u \r \e)
+(seq {:a 5 :b 6}) ; => ([:a 5] [:b 6])
+(seq (java.util.ArrayList. (range 5)))   ; => (0 1 2 3 4)
+(seq (into-array ["Awesome" "Clojure"])) ; => ("Awesome" "Clojure")
+(seq [])  ; => nil
+(seq nil) ; => nil
 
 ;;; Lazy sequences
 
