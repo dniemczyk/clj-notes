238373d22e891bcc963bdf879ba3f5ff7f919b2d
Damian Niemczyk <djniemczyk7@googlemail.com>
2015-10-03 15:30:32 +0200

    Format for beter readability

--- src/clj_notes/collections.clj
+++ src/clj_notes/collections.clj
@@ -10,13 +10,13 @@
 ;;; empty - get a empty instance of the same type as the provided collection
 ;;; =     - value equality of a collection to another (or many) collections
 
-;; conj allways adds a given value to the colection 'efficiently'
-;; for example for a list it prepends the value, because doing
-;; anything else would require traversing the list - a costly action
-;; if working with lage datasets
+;; conj - allways adds a given value to the colection 'efficiently'
+;;        for example for a list it prepends the value, because doing
+;;        anything else would require traversing the list - a costly action
+;;        if working with lage datasets
 (conj '(1 2 3) 4) ; => (4 1 2 3)
 
-;; into is build upon the conj and as such it will also add data
-;; in the most efficient way
+;; into - is build upon the conj and as such it will also add data
+;;        in the most efficient way
 (into '(1 2 3) [:a :b :c]) ; => (:c :b :a 1 2 3)
-(into [1 2 3] [:a :b :c]) ; => [1 2 3 :a :b :c]
+(into [1 2 3] [:a :b :c])  ; => [1 2 3 :a :b :c]
